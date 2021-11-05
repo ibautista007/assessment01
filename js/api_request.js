452cce9bdd76866f323149e7f2cbbfeeca8f106d
Isa Bautista <isaias.bautista@campus-support.ca>
2021-11-05 00:49:31 -0700

    first commit

---
+++ js/api_request.js
@@ -0,0 +1,17 @@
+$(function(){
+    var $users = $('#users');
+
+    $.ajax({
+        url: 'https://randomuser.me/api/?results=3',
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+          console.log(data.results);
+
+          $.each(data.results,function(i,user){
+                $users.append('<li>name:' + user.gender)
+          });
+        }
+      });
+
+});
