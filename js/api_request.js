b66c9a206861256c0558d9bab70fa1e066724784
Isa Bautista <isaias.bautista@campus-support.ca>
2021-11-05 15:48:09 -0700

    update form

--- js/api_request.js
+++ js/api_request.js
@@ -11,11 +11,7 @@ $(function(){
           console.log(data.results);
 
           $.each(data.results,function(i,user){
-                $users.append('<li>gender:' + user.name.title +' '+user.name.first +' '+user.name.last);
                 $staffs.append('<div class="card2"><div class="image2 d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="'+user.picture.thumbnail+'" height="100" width="100" /></button> <span class="name mt-3">'+user.name.title +' '+user.name.first +' '+user.name.last+'</span> <span class="idd">'+user.email+'</span> <div class=" px-2 rounded mt-4 date "><span class="join">Joined May,2021</span> </div></div></div>');
-
-/**/
-
 
           });
         }
--- mail.php
+++ mail.php
@@ -1,8 +1,11 @@
 <?php
 //ver3
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {
+/*Setting with Recaptcha*/
+/*if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])) {*/
+if ($_SERVER['REQUEST_METHOD'] === 'POST'])) {
     // Build POST request:
+    /*
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = '6Lf-uawZAAAAADHtI_2uCpd9VA5J_Co-zGhHGMag';
     $recaptcha_response = $_POST['recaptcha_response'];
@@ -10,9 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])
     // Make and decode POST request:
     $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
     $recaptcha = json_decode($recaptcha);
-
+    */
     // Take action based on the score returned:
-    if ($recaptcha->score >= 0.5) {
+    //if ($recaptcha->score >= 0.5) {
         // Verified - send email        
         $to      = 'anime.mxa@gmail.com';
         $name = $_POST["name"];
@@ -33,11 +36,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response'])
         mail($to, $subject, $message, $headers);
         header('Location: contact.html');
         exit;
+        /*
     } else {
         // Not verified - show form error        
         header('Location: contact_wrong.html');
         exit;
     }
+    */
 }
 
 ?>  
