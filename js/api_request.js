dffe88aedc6671bc982a35233a1b463d8fb46b3c
Isa Bautista <isaias.bautista@campus-support.ca>
2021-11-07 11:42:08 -0800

    update joined time of staff

--- js/api_request.js
+++ js/api_request.js
@@ -7,11 +7,23 @@ $(function(){
         url: 'https://randomuser.me/api/?results=3',
         dataType: 'json',
         success: function(data) {
-          console.log(data);
-          console.log(data.results);
+          //console.log(data);
+          //console.log(data.results);
 
           $.each(data.results,function(i,user){
-                $staffs.append('<div class="card2"><div class="image2 d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="'+user.picture.thumbnail+'" height="100" width="100" /></button> <span class="name mt-3">'+user.name.title +' '+user.name.first +' '+user.name.last+'</span> <span class="idd">'+user.email+'</span> <div class=" px-2 rounded mt-4 date "><span class="join">Joined May,2021</span> </div></div></div>');
+
+            const jsonTime = JSON.stringify(user.registered);
+            const time = jsonTime.substring(9, 19);
+            let joined = new Date(time.substring(0,4), (time.substring(5,7))-1, time.substring(8,10)) //Months number start from 0 (January)
+
+
+                $staffs.append('<div class="card2"><div class="image2 d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="'
+                +user.picture.thumbnail+'" height="100" width="100" /></button> <span class="name mt-3">'+user.name.title +' '+user.name.first +' '+user.name.last+'</span> <span class="idd">'
+                +user.email+'</span> <div class=" px-2 rounded mt-4 date "><span class="join">Joined '+
+                joined.toLocaleString('default', { month: 'long' })+', '+ joined.getFullYear()+'</span> </div></div></div>');
+
+
+              //console.log(typeof d);            
 
           });
         }
