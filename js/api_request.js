88acc0e066191b2c7a76b22363e2d989689e3fa8
Isa Bautista <isaias.bautista@campus-support.ca>
2021-11-08 01:04:36 -0800

    update design

--- js/api_request.js
+++ js/api_request.js
@@ -1,6 +1,5 @@
 $(function(){
     var $users = $('#users');
-
     var $staffs = $('#staffs');
 
     $.ajax({
@@ -11,17 +10,14 @@ $(function(){
           //console.log(data.results);
 
           $.each(data.results,function(i,user){
-
             const jsonTime = JSON.stringify(user.registered);
             const time = jsonTime.substring(9, 19);
             let joined = new Date(time.substring(0,4), (time.substring(5,7))-1, time.substring(8,10)) //Months number start from 0 (January)
-
 
                 $staffs.append('<div class="card2"><div class="image2 d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="'
                 +user.picture.thumbnail+'" height="100" width="100" /></button> <span class="name mt-3">'+user.name.title +' '+user.name.first +' '+user.name.last+'</span> <span class="idd">'
                 +user.email+'</span> <div class=" px-2 rounded mt-4 date "><span class="join">Joined '+
                 joined.toLocaleString('default', { month: 'long' })+', '+ joined.getFullYear()+'</span> </div></div></div>');
-
 
               //console.log(typeof d);            
 
